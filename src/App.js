a3551260c9d4bda24df0cbf93a63a0ac0d48f07a
bechara0 <jjbechara@gmail.come>
2023-07-15 08:49:21 -0300

    antes de probar getall

--- src/App.js
+++ src/App.js
@@ -15,13 +15,6 @@ const App = () => {
     });
   }, []);
 
-  // let guiaTel = [
-  //   { name: "Arto Hellas", number: "040-123456" },
-  //   { name: "Ada Lovelace", number: "39-44-5323523" },
-  //   { name: "Dan Abramov", number: "12-43-234345" },
-  //   { name: "Mary Poppendieck", number: "39-23-6423122" },
-  // ];
-
   return (
     <div>
       <h2>Phonebook</h2>
